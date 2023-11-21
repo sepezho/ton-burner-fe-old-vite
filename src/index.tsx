33e8e1815ad50233df5340ea0fc302a903fa54ef
sepezho <sepezho@gmail.com>
2023-11-21 23:46:11 +0300

    new endpoint for twa

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { BrowserRouter } from 'react-router-dom';
-import * as Buffer from 'buffer';
+import Buffer from 'buffer';
+//@ts-ignore
+globalThis.Buffer = Buffer;
+(window as any).global = window;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
